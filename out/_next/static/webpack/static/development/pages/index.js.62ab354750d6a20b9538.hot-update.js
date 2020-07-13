2c370e4203f2df38c034bdc24681ec4705e8a598
AlgoRithm <uzkano@mail.ru>
2020-07-13 18:26:23 +0500

    Initial out subtree commit gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.62ab354750d6a20b9538.hot-update.js
@@ -0,0 +1,93 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/picture.jsx":
+/*!********************************!*\
+  !*** ./components/picture.jsx ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/sarvar/Projects/ReactJs/react-portfolio/components/picture.jsx";
+
+
+
+var Picture = function Picture(_ref) {
+  var source = _ref.source,
+      alt = _ref.alt,
+      deployed = _ref.deployed,
+      github = _ref.github;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "picContainer",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: source,
+    alt: alt,
+    className: "image image-fluid",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "overlay",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: deployed,
+    target: "_blank",
+    rel: "noopener noreferrer",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Published"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: 'text',
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: deployed,
+    target: "_blank",
+    rel: "noopener noreferrer",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, alt))));
+};
+
+Picture.propTypes = {
+  source: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string,
+  deployed: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string,
+  alt: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string,
+  github: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string
+};
+Picture.defaultProps = {
+  source: '',
+  deployed: '',
+  alt: '',
+  github: ''
+};
+/* harmony default export */ __webpack_exports__["default"] = (Picture);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.62ab354750d6a20b9538.hot-update.js.map
