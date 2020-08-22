d89f998348dd88e9e3b770f14380169486272aa1
sarvarr <uzkano@mail.ru>
2020-08-22 18:18:02 +0500

    .........

--- components/footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => (
-  <footer className="footer">
-    <div className="container">
-      <p className="text-muted text-center">Sarvar Abdurahimov 2020</p>
-    </div>
-  </footer>
-);
-
-export default Footer;
